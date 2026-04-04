641faa7d2aeae444f6552bf32dbed5daaf27bbb4
Madeefar Abbas <madeefarabbas@gmail.com>
2026-04-05 03:05:55 +0400

    feat: implement server setup with Express and WebSocket integration

---
+++ backend/src/server.ts
@@ -0,0 +1,18 @@
+// src/server.ts
+
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const routes = require("./routes/ticker.routes");
+const { initWebSocket } = require("./websocket/ws");
+
+const app = express();
+app.use(cors());
+app.use("/api", routes);
+
+const server = http.createServer(app);
+initWebSocket(server);
+
+server.listen(5000, () => {
+  console.log("Backend running on port 5000");
+});
